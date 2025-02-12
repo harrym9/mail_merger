bc1cb5dd71cf860ecbd558c419a0463a848f1550
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-02-12 17:55:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+with open("Input/Letters/starting_letter.txt") as file:
+    letter = file.read()
+
+with open("Input/Names/invited_names.txt", "r") as file:
+    lines = file.readlines()
+    names = []
+    for line in lines:
+        names.append(line.strip("\n"))
+
+for name in names:
+    with open(f"Output/ReadyToSend/invite_for_{name}", "w") as file:
+        new_letter = letter.replace("[name],", name)
+        file.write(new_letter)
